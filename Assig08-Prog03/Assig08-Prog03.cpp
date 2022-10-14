13fddf5583f62d6723419c5a99473980e1a1e5c8
Vinhuanc <huann.chenn@gmail.com>
2022-10-13 22:15:37 -0500

    working program03

--- Assig08-Prog03/Assig08-Prog03.cpp
+++ Assig08-Prog03/Assig08-Prog03.cpp
@@ -29,3 +29,38 @@ int main(){
   
     }
 }
+
+//Program #3
+//Write a program that asks the user for the hours worked and pay rate for a predefined set of employee id numbers. After collecting this information, the program will calculate the wages for each employee and display the results in a table format. Your program will utilize 4 parallel arrays in order to achieve its goals:
+//• empId: an array of long integers to hold the employee’s id number. Will be initialized with { 5658845, 4520125, 7895122, 8451277, 1302850, 7580489 }
+//• hours: an array of integers to hold the number of hours worked by each employee
+//• payRate: an array of real numbers to hold the pay rate for each employee
+//• wages: an array of real numbers to hold the calculated wages for each employee
+//Sample Outputs:
+//Enter the requested information for each employee Employee #5658845
+//Hours worked: 24
+//Pay Rate: $12.54
+//Employee #4520125
+//Hours worked: 40
+//Pay Rate: $13.7
+//Employee #7895122
+//Hours worked: 37
+//Pay Rate: $16.84
+//Employee #8451277
+//Hours worked: 40
+//Pay Rate: $17.50
+//Employee #1302850
+//Hours worked: 35
+//Pay Rate: $22.45
+//Employee #7580489
+//Hours worked: 37
+//Pay Rate: $17.32 ---------------------------------
+//Employee Wages
+//---------------------------------
+//Employee #5658845 $ 300.96
+//Employee #4520125 $ 548.00
+//Employee #7895122 $ 623.08
+//Employee #8451277 $ 700.00
+//Employee #1302850 $ 785.75
+//Employee #7580489 $ 640.84
+//Press any key to continue . . .
